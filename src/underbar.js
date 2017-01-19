1c6e73cec690f879b65f0f16b5f3ddb1acac6612
Dimitri Temkin <me.dev30+hr@gmail.com>
2017-01-19 15:50:11 +1100

    small refactor of _.each

--- src/underbar.js
+++ src/underbar.js
@@ -55,7 +55,8 @@
       for (var i = 0; i < collection.length; i++) {
         iterator(collection[i], i, collection);
       }
-    } else if (isObject) {
+    }
+    if (isObject) {
       for (var key in collection) {
         iterator(collection[key], key, collection);
       }
